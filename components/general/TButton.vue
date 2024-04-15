920951b6d8358d7503b86869317eae0f796e509e
nataliiashavala <30192894@nescol.ac.uk>
2024-04-15 11:37:24 +0300

    fix: option button

--- components/general/TButton.vue
+++ components/general/TButton.vue
@@ -5,50 +5,36 @@
       buttonStyles.backgroundColor,
       buttonStyles.textColor,
       buttonStyles.borderColor
-    ]"
+    ] "
   >
-    <p class="font-nanum text-xl">{{ text }}</p> <!-- Allows for custom button text -->
+    <slot/>
   </button>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { defineProps, computed } from 'vue';
 
 const props = defineProps({
-  text: String,
   colorOption: {
     type: String,
     default: 'option1', // Default color option
-    validator: value => ['option1', 'option2', 'option3'].includes(value)
+    validator: value => ['option1', 'option2'].includes(value)
   }
 });
 
 const buttonStyles = computed(() => {
-  switch (props.colorOption) {
-    case 'option1':
-      return {
+  return props.colorOption === 'option1'
+    ? {
         backgroundColor: 'bg-[#545A3F]',
         textColor: 'text-white',
         borderColor: ''
-      };
-    case 'option2':
-      return {
+      }
+    : props.colorOption === 'option2'
+    ? {
         backgroundColor: 'bg-[#545A3F]',
         textColor: 'text-white',
         borderColor: 'border border-white'
-      };
-    case 'option3':
-      return {
-        backgroundColor: 'bg-[#F5F5F5]',
-        textColor: 'text-black',
-        borderColor: 'border border-black'
-      };
-    default:
-      return {}; // Fallback styles if needed
-  }
+      }
+    : {}; // Fallback styles if needed
 });
 </script>
-
-<style>
-/* Ensure Tailwind processes your custom colors */
-</style>
