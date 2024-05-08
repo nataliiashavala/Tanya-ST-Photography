e56e21002c8fbfde8025238a956ea8c4a6378c9e
nataliiashavala <30192894@nescol.ac.uk>
2024-05-08 10:20:10 +0100

    added content to pages

--- pages/trusted-suppliers/index.vue
+++ pages/trusted-suppliers/index.vue
@@ -1,5 +1,150 @@
+<script setup>
+import TCard from "~/components/general/TCard.vue";
+import heroImage from "~/assets/img/trusted_suppliers/hero_card_img.jpg";
+
+
+</script>
+
 <template>
-  <div>
-    <h1>About me</h1>
+  <div class="container mx-auto space-y-28">
+    <TCard
+      title="TRUSTED SUPPLIERS"
+      text="In the tapestry of life's most cherished events, every detail counts. That's why I've meticulously curated a list of trusted suppliers—each one committed to quality, reliability, and the fine art of crafting unforgettable experiences. From florists who weave emotion into every bouquet to caterers who bring a taste of the divine to your plate, my network of professionals is at your service.
+      When you choose to work with our recommended suppliers, you're not just selecting services, you're weaving a cohesive elegance into your event. Each element is synchronized for harmony, creating an orchestra of experiences that resonate with your theme and desires."
+      :image-src="heroImage"
+    />
+    <div class="flex justify-center">
+      <div class="flex flex-row space-x-64">
+        <div class ="flex flex-col space-y-12 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+          <div class="flex flex-col space-y-5">
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+          </div>
+        </div>
+        <div class ="flex flex-col space-y-12 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+          <div class="flex flex-col space-y-5">
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+          </div>
+        </div>
+        <div class ="flex flex-col space-y-12 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+          <div class="flex flex-col space-y-5">
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center">
+      <div class="flex flex-row space-x-64">
+        <div class ="flex flex-col space-y-12 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+          <div class="flex flex-col space-y-5">
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+          </div>
+        </div>
+        <div class ="flex flex-col space-y-12 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+          <div class="flex flex-col space-y-5">
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+          </div>
+        </div>
+        <div class ="flex flex-col space-y-12 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+          <div class="flex flex-col space-y-5">
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+            <a href="https://www.example.com" class="">LAZY SUNDAY</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center items-center">
+      <div class="w-72 h-px bg-black mr-4"></div>
+      <h1 class="text-6xl tracking-widest font-explora text-center">Handpicked for Excellence</h1>
+      <div class="w-72 h-px bg-black mr-4"></div>
+    </div>
+    <!-- Pictures of brans logo -->
+    <div class="flex flex-col justify-center items-center space-y-16 mb-24">
+      <div class="flex flex-row space-x-12">
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+      </div>
+      <div class="flex flex-row space-x-12">
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+        <div class ="flex flex-col text-center justify-center  space-y-7 text-2xl">
+          <h1 class="font-bold">VENUES</h1>
+            <a href="https://www.example.com" class="flex flex-col space-y-5">
+              <img src="/assets/img/trusted_suppliers/logo_sharpscot.jpg" alt="Logo of brand">
+            </a>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
+
+
