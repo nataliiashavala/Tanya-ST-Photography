2aa17015f880f5f42fd17513ef0680ac4c4912fb
nataliiashavala <30192894@nescol.ac.uk>
2024-05-13 08:58:42 +0100

    made a new component and added assets

---
+++ components/general/SocialMediaIcons.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex space-x-6"> 
+    <font-awesome-icon :icon="['fab', 'instagram']" class="hover:text-pink-700 instagram-icon" size="2x" />
+    <font-awesome-icon :icon="['fab', 'square-facebook']" class="text-black hover:text-blue-700" size="2x" />
+  </div>
+</template>
+
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSquareFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faSquareFacebook, faInstagram);
+</script>
+
+<style scoped>
+
+
+</style>
