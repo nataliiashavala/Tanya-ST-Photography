a51161485c0bf03ff10c48a97f7c204b345e9540
nataliiashavala <30192894@nescol.ac.uk>
2024-05-19 22:04:27 +0100

    import change

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Home from "~/pages/home_temp/index.vue";
 import Wedding from "~/pages/wedding_temp/index.vue";
-import Family from "~/pages/family/index.vue";
 import AboutMe from "~/pages/about-me/index.vue";
 import PersonalBrand from "~/pages/personal-brand/index.vue";
 </script>
