836d503a04cbfb39aea8ce1368b0aebf39d3a625
nataliiashavala <30192894@nescol.ac.uk>
2024-05-19 16:40:17 +0100

    changed names of folders

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Home from "~/pages/home/index.vue";
-import Wedding from "~/pages/wedding/index.vue";
+import Home from "~/pages/home_temp/index.vue";
+import Wedding from "~/pages/wedding_temp/index.vue";
 import Family from "~/pages/family/index.vue";
 import AboutMe from "~/pages/about-me/index.vue";
 import PersonalBrand from "~/pages/personal-brand/index.vue";
