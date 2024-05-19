06a45e62cbc562d28d99ecaed865937bd8ebc5ce
nataliiashavala <30192894@nescol.ac.uk>
2024-05-19 18:54:30 +0100

    added new page

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,5 +1,44 @@
+<script setup>
+
+</script>
+
 <template>
-  <div>
-    <h1>Portfolio</h1>
+  <div class="flex flex-col justify-center px-6 sm:px-8 md:px-20 lg:px-64 lg:pt-36">
+    <h1 class="text-3xl lg:text-6xl text-center lg:tracking-widest font-nanum mb-16 lg:mb-36">Portfolio</h1>
+  <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div class="grid gap-4">
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="/assets/img/portfolio/" alt="">
+          </div>
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
+          </div>
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
+          </div>
+      </div>
+      <div class="grid gap-4">
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
+          </div>
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
+          </div>
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
+          </div>
+      </div>
+      <div class="grid gap-4">
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
+          </div>
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
+          </div>
+          <div>
+              <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
+          </div>
+      </div>
+  </div>
   </div>
 </template>
