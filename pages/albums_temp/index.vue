f115c9ab770b3832b06831c26443eb97a0f67923
nataliiashavala <30192894@nescol.ac.uk>
2024-05-19 18:50:21 +0300

    fixed name

---
+++ pages/albums_temp/index.vue
@@ -0,0 +1,75 @@
+<script setup>
+import TButton from "~/components/general/TButton.vue";
+</script>
+<template>
+  <!-- First Section-->
+  <div class="flex justify-center px-64">
+    <div class="container space-y-56">
+      <div class="flex justify-center items-center">
+        <div class="text-right m-36">
+          <h1 class="text-custom tracking-widest font-nanum mb-36">
+            Your memories will find a beautiful home in our wedding albums.
+          </h1>
+            <div class="flex flex-row justify-center relative">
+              <img class ="absolute left-4 top-52" 
+              src="/assets/img/albums/hero_img_down.jpg" 
+              alt="Logo of brand">
+              <img class="" 
+              src="/assets/img/albums/hero_img_up.jpg" 
+              alt="Logo of brand">
+            </div>
+          </div>
+      </div>
+    
+    <!-- Second Section-->
+      <div class="flex justify-center items-center">
+      <div class="text-center space-y-20">
+        <h1 class="text-custom font-nanum">Velor</h1>
+        <p class="text-2xl">Not only the color of the material is important, but the feeling of touch, which conveys tenderness and warmth. Books made of velor are pleasant not only to look at, but also to hold in your hands. Velor has the unique feature of changing its texture when touched.</p>
+        <div class="flex flex-1 justify-center items-center"> 
+          <div class="flex flex-row space-x-7">
+            <div class="w-24 h-24 bg-[#A18080] rounded-full"></div>
+            <div class="w-24 h-24 bg-yellow-500 rounded-full"></div>
+            <div class="w-24 h-24 bg-[#F0ECE1] rounded-full"></div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <!-- Place for Album-->
+      <!-- Third Section-->
+      <div class="flex justify-center items-center">
+        <div class="text-center space-y-20 font-nanum">
+          <h1 class="text-custom ">DEFFE textiles</h1>
+          <p class="text-2xl">Not only the color of the material is important, but also the feeling of touch that conveys softness and warmth. Books made of velvet are pleasant not only to look at but also just to hold in your hands. Velvet has the unique characteristic of changing its texture upon touch.</p>
+          <div class="flex justify-center items-center"> 
+            <div class="flex flex-row space-x-7">
+              <div class="w-24 h-24 bg-zinc-300  rounded-full"></div>
+              <div class="w-24 h-24 bg-[#655636] rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Place for Album-->
+      <!-- Fourth Section-->
+      <div class="flex justify-center items-center">
+        <div class="text-center space-y-20">
+          <h1 class="text-custom font-nanum">Fine Art / Linen</h1>
+          <div class="flex justify-center items-center"> 
+            <div class="">
+              <div class="w-24 h-24 bg-[#ABA597] rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <p class="text-2xl font-nanum mb-20">"Begin the journey of preserving your cherished memories with our bespoke wedding albums. Prices start from £400 and vary depending on the design and materials chosen. Each album is crafted to reflect the unique beauty of your special day.”</p>
+      
+      <TButton 
+        color-option="option1">
+        Get In Touch
+      </TButton>
+      </div>
+    </div>
+  </div>
+
+</template>
---
+++ pages/maternity_temp/index.vue
@@ -0,0 +1,54 @@
+<script setup>
+import TCard from "~/components/general/TCard.vue";
+import heroImage from "~/assets/img/maternity/hero_card_img.jpg";
+import TRightCard from "~/components/general/TRightCard.vue";
+import TPriceCard from "~/components/general/TPriceCard.vue";
+import PriceCardImage from "~/assets/img/maternity/price_card.jpg";
+import TButton from "~/components/general/TButton.vue";
+import rightCardImage from "~/assets/img/maternity/right_card_img.jpg";
+
+const priceCardItems = [
+  "Pre-shoot consultation",
+  "2 hour shoot",
+  "300+ high-resolution, edited, digital photos",
+  "Online Gallery to vie",
+];
+</script>
+
+<template>
+  <div class="text-white" :style="{ backgroundColor: '#545A3F' }">
+    <div class="flex justify-center px-64 pt-36">
+      <div class="container space-y-32">
+        <TCard
+          title="MATERNITY"
+          addTitle="CARING. LOVING. CALM"
+          text="Embrace the beauty of your journey into motherhood with my Maternity Photoshoot Sessions. As you stand at the threshold of a new chapter, these sessions are crafted to honor the incredible transformation you are experiencing. In the tranquility of your chosen setting, we will collaborate to create graceful, tender portraits that capture the anticipation and love you hold for your unborn child. This is a time of wonder and change, and my maternity photoshoots are designed to reflect the strength and elegance of your pregnancy. With gentle direction and artistic vision, I'll capture the intimate bond between you and your baby-to-be."
+          :image-src="heroImage"
+        />
+
+        <TRightCard
+          first-title="EMOTIONS THROUGH EVERY PHOTO"
+          second-title="ON THE SHOOT"
+          :image-src="rightCardImage"
+          text="Whether It`s capturing the unique bond between a mother and a daughter: the laughter, the joy, those unexpected and natural moments between them, or a dog and its devoted owner taking a stroll through the park, or the love shared between a mother and father and their precious new baby, I love to show these moments through photos.
+          My preferred approach to any type of family shoot is relaxed, simple and unscripted: joining a family exploring the great outdoors, enjoying the simplicity of their daily routine… I love story-telling through photos. Memories that last a lifetime."
+        />
+
+        <div class="">
+          <TPriceCard
+            title-one="PRICING"
+            title="FAMILY SHOOT - £250"
+            :image-src="PriceCardImage"
+            :items="priceCardItems"
+            styleOption= "whiteOption"
+          />
+          <TButton 
+          class="outline outline-white text-center" 
+          color-option="option1">
+          Get in touch
+          </TButton>
+        </div>
+      </div>
+     </div>
+  </div>
+</template>
