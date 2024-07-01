a6f759245150f7cff4e4b03a4d6b302d1b9902af
“nataliiashavala” <30192894@nescol.ac.uk>
2024-07-01 12:41:46 +0300

    fixed header for momile view

--- components/general/THeader.vue
+++ components/general/THeader.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { faBlackTie } from '@fortawesome/free-brands-svg-icons';
-import { onMounted, onUnmounted, computed, ref, reactive } from 'vue';
+import { onMounted, onUnmounted, computed, ref, reactive, nextTick } from 'vue';
 import Logo from "~/assets/icons/logo.svg";
 import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
 const router = useRouter();
+
+const isMenuOpen = ref(false);
 
 const greenBackgroundRoutes = [
   "/wedding_temp",
@@ -28,7 +29,6 @@ const logoStyle = computed(() => ({
 const selectStyle = computed(() => ({
   backgroundColor: hasGreenBackground.value ? '#545A3F' : 'white',
   color: hasGreenBackground.value ? 'white' : 'black',
-
 }));
 
 const optionStyle = computed(() => ({
@@ -52,7 +52,7 @@ const links = [
       { name: "babies", path: "/babies_temp" },
       { name: "maternity", path: "/maternity_temp" },
       { name: "portfolio", path: "/portfolio" },
-      { name: "trusted supliers", path: "/trusted-suppliers" },
+      { name: "trusted suppliers", path: "/trusted-suppliers" },
       { name: "blog", path: "/blog" },
       { name: "albums", path: "/albums_temp" },
     ],
@@ -60,10 +60,18 @@ const links = [
 ];
 const value = ref("");
 
-const handleSelectChange = (event) => {
-  const value = event.target.value;
+const handleSelectChange = (value) => {
   if (value) {
     router.push(value);
+    closeSidebar();
+  }
+};
+
+const closeSidebar = () => {
+  const sidebar = document.getElementById('sidebar');
+  if (sidebar) {
+    sidebar.classList.add('-translate-x-full');
+    isMenuOpen.value = false;
   }
 };
 
@@ -90,98 +98,96 @@ onUnmounted(() => {
   }
 });
 
-
-onMounted(() => {
+onMounted(async () => {
+  await nextTick();
   const sidebar = document.getElementById('sidebar');
   const openSidebarButton = document.getElementById('open-sidebar');
-    
+  
+  if (sidebar && openSidebarButton) {
     openSidebarButton.addEventListener('click', (e) => {
-        e.stopPropagation();
-        sidebar.classList.toggle('-translate-x-full');
+      e.stopPropagation();
+      sidebar.classList.toggle('-translate-x-full');
+      isMenuOpen.value = !isMenuOpen.value;
     });
 
     // Close the sidebar when clicking outside of it
     document.addEventListener('click', (e) => {
-        if (!sidebar.contains(e.target) && !openSidebarButton.contains(e.target)) {
-            sidebar.classList.add('-translate-x-full');
-        }
+      if (!sidebar.contains(e.target) && !openSidebarButton.contains(e.target)) {
+        closeSidebar();
+      }
     });
-  });
+  }
+});
+
 </script>
 
 <template>
-
-  <div
-    :class="{ 'bg-customGreen text-white': hasGreenBackground }"
-    class="header flex items-end uppercase pt-0 lg:pt-9 pl-4 lg:pl-24 lg:pr-64 pb-14 lg:pb-0 font-lato tracking-widest">
-  
+  <div :class="{ 'bg-customGreen text-white': hasGreenBackground }" class="header pl-20">
 
     <!-- Sidebar -->
-  <div class="absolute inset-y-0 left-0 bg-[#F5F5F5] text-black w-80 min-h-screen overflow-y-auto z-50 transition-transform transform -translate-x-full ease-in-out duration-300"
-      id="sidebar">
-        <!-- Your Sidebar Content -->
-              <div class="p-4">
-                  <ul class="mt-4 ml-11 text-sm font-nanum uppercase flex flex-col space-y-8 cursor-pointer">
-                      <nuxt-link to="/home_temp" class="">home</nuxt-link>
-                      <nuxt-link to="/wedding_temp" class="">wedding</nuxt-link>
-                      <nuxt-link to="/family" class="">family</nuxt-link>
-                      <nuxt-link to="/personal-brand" class="">personal brand</nuxt-link>
-                      <nuxt-link to="/about-me" class="">about me</nuxt-link>
-                      <nuxt-link to="/solo_temp" class="">solo</nuxt-link>
-                      <nuxt-link to="/elopements" class="">elopements</nuxt-link>
-                      <nuxt-link to="/couples_temp" class="">couples</nuxt-link>
-                      <nuxt-link to="/boudoir" class="">boudoir</nuxt-link>
-                      <nuxt-link to="/babies_temp" class="">babies</nuxt-link>
-                      <nuxt-link to="/maternity_temp" class="">maternity</nuxt-link>
-                      <nuxt-link to="/albums_temp" class="">albums</nuxt-link>
-                      <nuxt-link to="/porfolio" class="">portfolio</nuxt-link>
-                      <nuxt-link to="/trusted-suppliers" class="">trusted suppliers</nuxt-link>
-                      <nuxt-link to="/blog" class="">blog</nuxt-link>
-                      <nuxt-link to="/contact-me" class="">contact me</nuxt-link>
-                  </ul>
-              </div>
-  </div>
-
-
-  <div class="flex items-end justify-between lg:justify-start">
-    <div class="pt-5 md:pt-0 ml-2 mr-0 lg:ml-24 lg:mr-24 ">
-    <nuxt-link to="/home_temp" class="">
-      <img :src="Logo" alt="Logo" :style="logoStyle" class="w-16 h-16 lg:w-full lg:h-full" />
-    </nuxt-link></div>
-    <div class="">
-    <nuxt-link class="ml-40 mr-0 text-2xl uppercase font-nanum block lg:hidden cursor-pointer" id="open-sidebar">
-      menu
-    </nuxt-link>
-  </div>
-  
-    <nav :class="{ 'open': isMenuOpen }" class="flex flex-row  whitespace-nowrap items-center text-xs lg:border-b hidden lg:block">
-      <div v-for="(item, index) in links" :key="`link-${index}`" v-if="isMenuOpen || windowSize.width >= 900" class="flex">
-        <nuxt-link v-if="item.path" :to="item.path" class="text-xs mr-24 font-lato tracking-widest mb-4 uppercase">
-          {{ item.name }}
-        </nuxt-link>
+    <div class="absolute inset-y-0 left-0 bg-[#F5F5F5] text-black w-80 min-h-screen overflow-y-auto z-50 transition-transform transform -translate-x-full ease-in-out duration-300" id="sidebar">
+      <div class="p-4">
+        <ul class="mt-4 ml-10 text-sm font-nanum uppercase flex flex-col space-y-8 cursor-pointer">
+          <nuxt-link to="/home_temp" class="" @click="closeSidebar">home</nuxt-link>
+          <nuxt-link to="/wedding_temp" class="" @click="closeSidebar">wedding</nuxt-link>
+          <nuxt-link to="/family" class="" @click="closeSidebar">family</nuxt-link>
+          <nuxt-link to="/personal-brand" class="" @click="closeSidebar">personal brand</nuxt-link>
+          <nuxt-link to="/about-me" class="" @click="closeSidebar">about me</nuxt-link>
+          <nuxt-link to="/solo_temp" class="" @click="closeSidebar">solo</nuxt-link>
+          <nuxt-link to="/elopements" class="" @click="closeSidebar">elopements</nuxt-link>
+          <nuxt-link to="/couples_temp" class="" @click="closeSidebar">couples</nuxt-link>
+          <nuxt-link to="/boudoir" class="" @click="closeSidebar">boudoir</nuxt-link>
+          <nuxt-link to="/babies_temp" class="" @click="closeSidebar">babies</nuxt-link>
+          <nuxt-link to="/maternity_temp" class="" @click="closeSidebar">maternity</nuxt-link>
+          <nuxt-link to="/albums_temp" class="" @click="closeSidebar">albums</nuxt-link>
+          <nuxt-link to="/portfolio" class="" @click="closeSidebar">portfolio</nuxt-link>
+          <nuxt-link to="/trusted-suppliers" class="" @click="closeSidebar">trusted suppliers</nuxt-link>
+          <nuxt-link to="/blog" class="" @click="closeSidebar">blog</nuxt-link>
+          <nuxt-link to="/contact-me" class="" @click="closeSidebar">contact me</nuxt-link>
+        </ul>
       </div>
-      <div class="relative custom-select-wrapper mr-20" v-if="isMenuOpen || windowSize.width >= 900">
-        <select 
-          @change="handleSelectChange" 
-          v-model="value" 
-          :style="selectStyle" 
-          class="custom-select w-20 uppercase text-xs mb-4"
-        >
-          <option value="" disabled>MORE</option>
-          <option 
-            v-for="(option, index) in links.find((link) => link.name === 'more')?.more ?? []" 
-            :key="`more-${index}`" 
-            :value="option.path" 
-            :style="optionStyle"
-            class="font-lato text-xs uppercase tracking-widest "
-          >
-            {{ option.name }}
-          </option>
-        </select>
+    </div>
+
+    <div class="flex items-end justify-between">
+      <div class="flex justify-between items-end">
+        <div class="mt-8 ml-24 mr-0 lg:ml-2 lg:mr-32">
+          <nuxt-link to="/home_temp">
+            <img :src="Logo" alt="Logo" :style="logoStyle" class="w-16 h-16 lg:w-full lg:h-full" />
+          </nuxt-link>
+        </div>
+        <div class="ml-auto">
+          <nuxt-link class="mb-3 ml-52 text-2xl uppercase font-nanum block lg:hidden cursor-pointer" id="open-sidebar" @click="isMenuOpen = !isMenuOpen">
+            menu
+          </nuxt-link>
+        </div>
       </div>
-      <nuxt-link to="/contact-me" class="mb-4 uppercase font-lato text-xs" v-if="isMenuOpen || windowSize.width >= 900">contact me</nuxt-link>
-    </nav>
-  </div>
+      <nav class="flex flex-row whitespace-nowrap gap-20 items-center text-xs lg:border-b lg:hidden ">
+        <div v-for="(item, index) in links" :key="`link-${index}`" v-if="isMenuOpen || windowSize.width >= 1024" class="flex">
+          <nuxt-link v-if="item.path" :to="item.path" class="text-xs font-lato tracking-widest uppercase mb-0">
+            {{ item.name }}
+          </nuxt-link>
+          <template v-if="item.more">
+            <el-select 
+              v-model="value" 
+              @change="handleSelectChange" 
+              :style="selectStyle"
+              class="custom-select w-full uppercase text-xs"
+              :popper-class="selectStyle"
+            >
+              <el-option 
+                v-for="(option, index) in item.more" 
+                :key="`more-${index}`"
+                :value="option.path" 
+                :label="option.name"
+                :style="optionStyle"
+                class="custom-option"
+              />
+            </el-select>
+          </template>
+        </div>
+        <nuxt-link to="/contact-me" class="uppercase font-lato text-xs hidden lg:block">contact me</nuxt-link>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -203,19 +209,84 @@ nav.open {
   max-height: 500px; /* Adjust as needed */
 }
 
-@media (min-width: 900px) {
+@media (min-width: 1024px) {
   nav {
     max-height: none;
   }
 }
 
-@media (max-width: 900px) {
+@media (max-width: 1024px) {
   nav {
     flex-direction: column;
     align-items: flex-start;
   }
   nav.open {
     display: flex;
+  }
+}
+
+.custom-select {
+  color: white !important;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  text-transform: uppercase;
+  font-family: 'Nanum Gothic', sans-serif;
+  font-size: 12px;
+  width: 150px;
+  margin: 0; /* Remove any default margin */
+}
+
+.custom-option {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-family: 'Nanum Gothic', sans-serif;
+  display: flex;
+  align-items: center;
+}
+
+.el-select .el-input__inner {
+  background-color: transparent !important;
+  border: none !important;
+  box-shadow: none !important;
+}
+
+.el-select-dropdown__item.selected {
+  background-color: #43493A !important;
+  color: white !important;
+}
+
+.el-select-dropdown__item:hover {
+  background-color: #43493A !important;
+  color: white !important;
+}
+
+.el-select-dropdown__item {
+  font-family: 'Nanum Gothic', sans-serif;
+}
+
+.el-select-dropdown__list {
+  padding: 0 !important;
+}
+
+.no-border {
+  border: none !important;
+}
+
+.no-shadow {
+  box-shadow: none !important;
+}
+</style>
+
+<style lang="scss">
+.custom-select {
+  .el-select__wrapper {
+    background-color: transparent !important;
+    border: none !important;
+    box-shadow: none !important;
+    align-items: center;
+    gap: 0 !important;
+    padding: 0 !important;
+    text-align: center;
   }
 }
 </style>
